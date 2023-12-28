af072d0f1bc26c6b20dcaee29c3afb39f4b8a566
SaiKiranPogula1500 <155062151+SaiKiranPogula1500@users.noreply.github.com>
2023-12-28 15:19:11 +0530

    commit-3

--- java code-0.java
+++ java code-0.java
@@ -12,7 +12,7 @@ public class ArrayListDemoClass {
 		al.add(3);
 		al.add(4);
 		al.add(1,"sai");
-                al.add(5);
+                al.add(6);
 		
         System.out.println(al.size());
         System.out.println(al.get(4));
